a175684ba69974eef29acef8d4ff8f463b7dedf7
afifafauziyah <afifahzyh@gmail.com>
2023-04-18 23:08:05 +0800

    +

---
+++ kolaborator.ino
@@ -0,0 +1,9 @@
+int led2Pin = 18; // Pin LED 2
+
+void setup() {
+  pinMode(led2Pin, OUTPUT); // Tentukan pin LED 2 sebagai output
+}
+
+void loop() {
+  digitalWrite(led2Pin, HIGH); // Nyalakan LED 2
+}
