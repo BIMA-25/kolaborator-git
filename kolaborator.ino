e2ab7ee8d375db1d49213d3ca957e8c49a1c671d
Intanchyaa <130478222+Intanchyaa@users.noreply.github.com>
2023-04-18 22:52:46 +0700

    +

---
+++ kolaborator.ino
@@ -0,0 +1,11 @@
+const int pin_led1 = 5; 
+
+void setup(){
+pinMode( pin_led1, OUTPUT); // set pin 5 sebagai output
+
+}
+
+void loop(){
+digitalWrite(pin_led1, HIGH); //menyalakan LED melalui pin 15 dengan value HIGH
+
+}
